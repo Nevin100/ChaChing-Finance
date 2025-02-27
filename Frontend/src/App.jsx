67c4e5ca700f479aba9b1fa8888a14f5009abf19
Nevin-Bali100 <opopopopop100000@gmail.com>
2025-02-27 22:04:13 +0530

    new commits

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,41 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./Pages/Auth/Login.jsx";
+import Register from "./Pages/Auth/Register.jsx";
+import Home from "./Pages/Dashboard/Home.jsx";
+import Income from "./Pages/Dashboard/Income.jsx";
+import Expense from "./Pages/Dashboard/Expense.jsx";
+
 const App = () => {
   return (
     <>
-      <h1>Hello APP</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
     </>
   );
 };
 
 export default App;
+
+export const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to={"/dashboard"} />
+  ) : (
+    <Navigate to={"/login"} />
+  );
+};
--- Frontend/src/Pages/Auth/Login.jsx
+++ Frontend/src/Pages/Auth/Login.jsx
@@ -1,7 +1,12 @@
 const Login = () => {
   return (
     <div>
-      <h1>Login</h1>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+      </div>
     </div>
   );
 };
